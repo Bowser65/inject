c98d271a6ac5e676dfadff58500ffe6e4c4a8c04
Bowser65 <contact.bowser65@gmail.com>
2017-08-27 14:25:38 +0200

    Reformat

--- modules/devtools.js
+++ modules/devtools.js
@@ -1,75 +1,75 @@
-const electron = require("electron")
-const download = require("download")
-const decompress = require("decompress")
+const electron = require('electron')
+const download = require('download')
+const decompress = require('decompress')
 
 class Devtools {
-    constructor(plugin) {
-        this.plugin = plugin
-        this.window = electron.remote.getCurrentWindow();
-        this.listener = this.listener.bind(this)
-        if (this.plugin.settings.devtools === false) return
+  constructor (plugin) {
+    this.plugin = plugin
+    this.window = electron.remote.getCurrentWindow()
+    this.listener = this.listener.bind(this)
+    if (this.plugin.settings.devtools === false) return
 
-        const extensionPath = this.extensionPath = _path.join(__dirname, "..", "extension")
-        if (!_fs.existsSync(extensionPath)) _fs.mkdirSync(extensionPath)
+    const extensionPath = this.extensionPath = _path.join(__dirname, '..', 'extension')
+    if (!_fs.existsSync(extensionPath)) _fs.mkdirSync(extensionPath)
 
-        Promise.resolve(_fs.existsSync(_path.join(extensionPath, "manifest.json")))
-            .then(exists => {
-                if (!exists) {
-                    this.plugin.debug("downloading extension...", this.extensionURL)
-                    return download(this.extensionURL)
-                        .then(buffer => {
-                            this.plugin.debug("extension downloaded, commencing the extraction")
-                            if (buffer.compare(Buffer.from("Cr24", "utf-8"), 0, 4, 0, 4) !== 0) {
-                                throw new Error("File has an invalid header. Skipping!")
-                            }
+    Promise.resolve(_fs.existsSync(_path.join(extensionPath, 'manifest.json')))
+      .then(exists => {
+        if (!exists) {
+          this.plugin.debug('downloading extension...', this.extensionURL)
+          return download(this.extensionURL)
+            .then(buffer => {
+              this.plugin.debug('extension downloaded, commencing the extraction')
+              if (buffer.compare(Buffer.from('Cr24', 'utf-8'), 0, 4, 0, 4) !== 0) {
+                throw new Error('File has an invalid header. Skipping!')
+              }
 
-                            if (buffer.readUInt32LE(4) != 2) {
-                                throw new Error("File uses an unsupported extension format", buffer)
-                            }
+              if (buffer.readUInt32LE(4) != 2) {
+                throw new Error('File uses an unsupported extension format', buffer)
+              }
 
-                            const pubKeyLen = buffer.readUInt32LE(8);
-                            const sigKeyLen = buffer.readUInt32LE(12);
-                            const zip = buffer.slice(16 + pubKeyLen + sigKeyLen)
-                            return decompress(zip, extensionPath, {
-                                map: file => {
-                                    if (file.path.endsWith("/")) {
-                                        file.type = "directory"
-                                    }
-                                    return file
-                                }
-                            })
-                        })
-                        .then(() => {
-                            this.plugin.debug("extension extracted!")
-                        })
+              const pubKeyLen = buffer.readUInt32LE(8)
+              const sigKeyLen = buffer.readUInt32LE(12)
+              const zip = buffer.slice(16 + pubKeyLen + sigKeyLen)
+              return decompress(zip, extensionPath, {
+                map: file => {
+                  if (file.path.endsWith('/')) {
+                    file.type = 'directory'
+                  }
+                  return file
                 }
+              })
             })
             .then(() => {
-                const manifest = this.manifest = require(_path.join(extensionPath, "manifest.json"))
-                this.window.webContents.on("devtools-opened", this.listener)
-                this.listener()
-
-                this.plugin.debug(`Detected ${manifest.name} v${manifest.version}.`)
+              this.plugin.debug('extension extracted!')
             })
-            .catch(ex => this.plugin.error("DevTools Extension:", ex))
-    }
+        }
+      })
+      .then(() => {
+        const manifest = this.manifest = require(_path.join(extensionPath, 'manifest.json'))
+        this.window.webContents.on('devtools-opened', this.listener)
+        this.listener()
 
-    get extensionURL() {
-        const chromeVersion = process.versions.chrome
-        const extensionID = 'fmkadmapgofadopljbjfkapdkoienihi'
-        return `https://clients2.google.com/service/update2/crx?response=redirect&prodversion=${chromeVersion}&x=id%3D${extensionID}%26uc`
-    }
+        this.plugin.debug(`Detected ${manifest.name} v${manifest.version}.`)
+      })
+      .catch(ex => this.plugin.error('DevTools Extension:', ex))
+  }
 
-    unload() {
-        electron.remote.BrowserWindow.removeDevToolsExtension(this.manifest.name);
-        this.window.webContents.removeListener("devtools-opened", this.listener)
-    }
+  get extensionURL () {
+    const chromeVersion = process.versions.chrome
+    const extensionID = 'fmkadmapgofadopljbjfkapdkoienihi'
+    return `https://clients2.google.com/service/update2/crx?response=redirect&prodversion=${chromeVersion}&x=id%3D${extensionID}%26uc`
+  }
 
-    listener() {
-        electron.webFrame.registerURLSchemeAsSecure("chrome-extension");
-        electron.remote.BrowserWindow.removeDevToolsExtension(this.manifest.name)
-        electron.remote.BrowserWindow.addDevToolsExtension(this.extensionPath)
-    }
+  unload () {
+    electron.remote.BrowserWindow.removeDevToolsExtension(this.manifest.name)
+    this.window.webContents.removeListener('devtools-opened', this.listener)
+  }
+
+  listener () {
+    electron.webFrame.registerURLSchemeAsSecure('chrome-extension')
+    electron.remote.BrowserWindow.removeDevToolsExtension(this.manifest.name)
+    electron.remote.BrowserWindow.addDevToolsExtension(this.extensionPath)
+  }
 }
 
 module.exports = Devtools
--- modules/util.js
+++ modules/util.js
@@ -1,16 +1,18 @@
 class Util {
-    constructor() {}
-    load() {}
-    unload() {}
+  constructor () {}
 
-    first(iterator, process) {
-        if (!process) process = () => true
+  load () {}
 
-        for (let child of iterator) {
-            const retVal = process(child)
-            if (retVal !== undefined) return retVal
-        }
+  unload () {}
+
+  first (iterator, process) {
+    if (!process) process = () => true
+
+    for (let child of iterator) {
+      const retVal = process(child)
+      if (retVal !== undefined) return retVal
     }
+  }
 }
 
 module.exports = Util
